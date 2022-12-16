fa4e5a8215375d7a732b74e8e12a453e951d1e8e
lunarwar2 <101807623+lunarwar2@users.noreply.github.com>
2022-12-16 21:01:20 +0700

    Create satunamtigaratus.js

---
+++ satunamtigaratus.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'bd841a74221ea7f7aa7c3181587c405a',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://hailofficemeasure.com/bd841a74221ea7f7aa7c3181587c405a/invoke.js"></scr' + 'ipt>');
